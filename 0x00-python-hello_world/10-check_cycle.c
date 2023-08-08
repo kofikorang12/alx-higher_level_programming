9532176237bd9918f8e6a23c0bd6f3918f39c9a3
Jeffrey Baah <jbaah69@gmail.com>
2023-08-08 01:42:33 +0000

    easter egg, check cycle, write, compile, and magic calculation

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a single linked list
+ * @list: list to be checked
+ *
+ * Return: 0 if no cycle, 1 if cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	if (list == NULL)
+		return (0);
+	
+	if (list->next == list)
+		return (1);
+
+	while (fast != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next;
+		if (fast != NULL)
+			fast = fast->next;
+		else
+			return (0);
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
