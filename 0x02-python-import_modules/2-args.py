8d700992b357c4382ab382c249549f8c6682999b
Jeffrey Baah <jbaah69@gmail.com>
2023-08-11 09:19:04 +0000

    import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.arg) == 1:
+        print("0 arguments.")
+    else:
+        if len(sys.arg) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(len(sys.arg) -1))
+                    a = 0
+                    for arr in sys.arg:
+                    if a != 0:
+                    print("{} : {}".format(a, arr))
+                    a = a + 1
---
+++ 0x02-python-import_modules/3-infinte_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    bool = 0
+    for arr in argv:
+        if bool == 1:
+            add = add + int(arr)
+            bool = 1
+
+            print(sum)
