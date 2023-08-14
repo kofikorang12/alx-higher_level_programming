dacce02b9959fea3b8353dab7af171e331acb495
Jeffrey Baah <jbaah69@gmail.com>
2023-08-14 17:03:04 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-firstletter = sentence[0]
-if sentence else None
-sentlen = len(sentence)
-return (sentlen, firstletter)
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
