312efa675602b36762ef305fda94028083b208f6
Jeffrey Baah <jbaah69@gmail.com>
2023-08-16 14:57:09 +0000

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,18 @@
 def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, str) is not True:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dicta = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
     tot = 0
     prev = 0
     for let in roman_string:
-        if let not in roman:
+        if let not in dicta:
             return 0
-        if roman[let] > tot:
-            tot = roman[let] - tot
-        elif roman[let] > prev:
-            tot = tot + roman[let] - prev * 2
+        if dicta[let] > tot:
+            tot = dicta[let] - tot
+        elif dicta[let] > prev:
+            tot = tot + dicta[let] - prev * 2
         else:
-            tot = roman[let] + tot
-            prev = roman[let]
-        return tot
+            tot = dicta[let] + tot
+        prev = dicta[let]
+    return tot
