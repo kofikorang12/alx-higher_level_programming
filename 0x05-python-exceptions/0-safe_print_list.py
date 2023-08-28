67d3d0578f2b9de134b372d3971b08d0c5a14acc
Jeffrey Baah <jbaah69@gmail.com>
2023-08-28 16:28:13 +0000

    0 safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    tot = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            tot = tot + 1
+        except:
+            pass
+
+    print()
+    return tot
