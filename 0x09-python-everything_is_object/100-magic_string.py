cff4390ee8eae9fbf361794cce009193a8edfeb7
Jeffrey Baah <jbaah69@gmail.com>
2023-09-06 23:40:16 +0000

    ans

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(counter=[0]):
-    counter[0] += 1
-    return ', '.join(['BestSchool'] * counter[0])
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
