2cf117ee6d9dbe76ffe5225ff9ebc3ac83115bba
Jeffrey Baah <jbaah69@gmail.com>
2023-09-11 12:54:00 +0000

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-''' module that contains a list subclass '''
+"""MyList Module"""
 
 
 class MyList(list):
-    ''' prints a sorted version of the list '''
+    """MyList class"""
+
     def print_sorted(self):
-        sorted_list = self.copy()
-        sorted_list.sort()
-        print(sorted_list)
-        del sorted_list
+        """Prints a sorted list"""
+        print(sorted(self))
