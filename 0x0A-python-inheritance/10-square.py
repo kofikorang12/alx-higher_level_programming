603c13596b1c0034fa80555eeaf1b7dbb80f82d9
Jeffrey Baah <jbaah69@gmail.com>
2023-09-11 11:54:07 +0000

    my list

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' module containing a rectangle class which inherits from rectangle'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' a square class which inherits from rectangle'''
+
+    def __init__(self, size):
+        ''' initializer for square '''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' module containing a rectangle class which inherits from rectangle'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' a square class which inherits from rectangle'''
+
+    def __init__(self, size):
+        ''' initializer for square '''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        ''' printable string of square '''
+        return '[Square] {}/{}'.format(self.__size, self.__size)
