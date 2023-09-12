623f538b104fb30621d21d4152868d3551b4edd8
Jeffrey Baah <jbaah69@gmail.com>
2023-09-12 10:10:22 +0000

    student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' function containing a class student '''
+
+
+class Student():
+    ''' the class student, with initializer, to_json '''
+    def __init__(self, first_name, last_name, age):
+        ''' initializer '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' creates a dict of instance attributes, handy for json '''
+        if isinstance(attrs, list) is True:
+            my_dict = dict()
+            for key in attrs:
+                if key in self.__dict__:
+                    my_dict[key] = self.__dict__[key]
+            return my_dict
+        return self.__dict__
