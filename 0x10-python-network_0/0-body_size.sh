3c8b9fa577eb2b05e820cd92e2ff22cdded8b89d
Kofi korang <jbaah69@gmail.com>
2023-11-30 18:44:44 +0000

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that shows the Content-Length from a HTTP request
-curl -sIXGET $1 | grep Content-Length | cut -d" " -f 2
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
