8098bf8d94fc1671ffb9ffb0153fd91aa51fe027
Kofi korang <jbaah69@gmail.com>
2023-12-01 23:52:58 +0000

    netwok

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""UrlLibRequest"""
-from urllib import parse, request
-import sys
+"""displays the value of the X-Request-Id variable found in
+the header of the res.
+"""
 
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from sys import argv
 
-if __name__ == '__main__':
-    data = parse.urlencode({"email": sys.argv[2]})
-    data = data.encode()
-    with request.urlopen(sys.argv[1], data) as res:
-        body = res.read()
-    print(body.decode("utf8"))
+    value = {"email": argv[2]}
+    request = Request(
+            argv[1], urlencode(value).encode("ascii"))
+    with urlopen(request) as response:
+        head = response.headers.get('X-Request-Id')
+        print(response.read().decode('utf-8'))
