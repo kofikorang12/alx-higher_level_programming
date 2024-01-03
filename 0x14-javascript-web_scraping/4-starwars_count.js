ab7bc7f56278f56fe10e131b491526a2228e2f2b
Kofi korang <jbaah69@gmail.com>
2024-01-03 15:54:05 +0000

    javaScript web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  let total = 0;
+  let resBody = JSON.parse(body).results;
+  for (let i in resBody) {
+    let characters = resBody[i]['characters'];
+    for (let j in characters) {
+      if (characters[j].search('18') !== -1) {
+        total++;
+        break;
+      }
+    }
+  }
+  console.log(total);
+});
